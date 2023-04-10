70dbeea476b61743d665fbffe03d72949fb2dc4b
viniciuss <109877390+vinixhq@users.noreply.github.com>
2023-04-10 12:26:48 -0300

    Add files via upload

---
+++ Arrays/script.js
@@ -0,0 +1,110 @@
+// const users = ["Ana", "Fernando", "Rafael", "Clara", "Maria"];
+
+// users.push("Roger", "Vinicius");
+
+// console.log(users);
+
+// --------------------------------------------------------
+
+// const person = ["Felipe", 30, "São Paulo", "Masculino", "584651671658"];
+
+// person.pop();
+
+// console.log(person);
+
+// --------------------------------------------------------
+
+// const fruits = ["Apple", "Orange", "Banana", "Pear"];
+
+// fruits.shift();
+
+// console.log(fruits);
+
+// --------------------------------------------------------
+
+// const jobs = ["Frontend", "Backend", "Devops", "FullStack"];
+
+// jobs.unshift("Design");
+
+// console.log(jobs);
+
+// --------------------------------------------------------
+
+// const jobs = ["Frontend", "Backend", "Devops", "FullStack"];
+// let indexOf = jobs.indexOf("Frontend");
+
+// console.log(indexOf);
+
+// --------------------------------------------------------
+
+// const fruits = ["Apple", "Orange", "Banana", "Pear"];
+// let indexOfSearch = fruits.indexOf("Orange", 3);
+
+// console.log(indexOfSearch);
+
+// --------------------------------------------------------
+
+// const jobs = ["Frontend", "Backend", "DevOps", "FullStack"];
+
+// jobs.sort();
+
+// console.log(jobs);
+
+// --------------------------------------------------------
+
+// const colors = ["Red", "Green", "White", "Black"];
+
+// let join = colors.join(",");
+
+// console.log(join);
+
+// --------------------------------------------------------
+
+// Escreva um script no javascript para unir todos os elementos do array:
+
+// const elements = ["Elemento1", "Elemento2", "Elemento3", "Elemento4", "Elemento5"];
+
+// elements.push("Elemento6", "Elemento7");
+
+// console.log(elements);
+
+// --------------------------------------------------------
+
+// Escreva um script que contenha uma lista de usuarios, ordenada por ordem alfabética:
+
+// const names = ["Ana", "Fernando", "Rafael", "Clara", "Maria"];
+
+// names.sort();
+
+// console.log(names);
+
+// --------------------------------------------------------
+
+// Escreva uma função que recebe duas strings, digamos a e b, como argumentos
+// Se a contém b, anexe b ao inicio de a, se não, anexe b ao final e retorne
+// a concatenação.
+
+// function myFunction(a, b) {
+//     let array = [];
+
+//     if(a.indexOf(b) !== -1){
+//         array.push(b + a);
+//         return array;
+//     } else {
+//         array.push(a + b);
+//         return array;
+//     }
+// }
+// console.log(myFunction("tips", "a"));
+
+// --------------------------------------------------------
+
+// Escreva um script que remova o primeiro elemento de um Array.
+
+// const phone = ["Positivo", "Xiaomi", "Samsung", "Apple"];
+
+// phone.shift();
+
+// console.log(phone);
+
+// --------------------------------------------------------
